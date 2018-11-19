20102aa44fff8c7dbec63706a3dca0e7e97af025
Robert Stine <RobWStine@outlook.com>
2018-11-19 15:33:04 -0500

    Tomcat update - Update pom to war and MainApp to use tomcat.

--- src/main/java/com/connector/qq/MainApp.java
+++ src/main/java/com/connector/qq/MainApp.java
@@ -5,12 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApp  {
+public class MainApp extends SpringBootServletInitializer  {
 
+    /*
+     * Uncomment main method and delete SpringBootServletInitializer to package as .jar / stand alone micro-service.
+     * */
 
+   /*
     public static void main(String[] args) {
 
         SpringApplication.run(MainApp.class, args);
     }
-
+    */
 }
