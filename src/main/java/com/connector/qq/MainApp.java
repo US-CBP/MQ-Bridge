959c4eb152872d6aa2cedb1389df3e36386debf0
Robert Stine <RobWStine@outlook.com>
2018-11-29 12:46:30 -0500

    Updating Main

--- src/main/java/com/connector/qq/MainApp.java
+++ src/main/java/com/connector/qq/MainApp.java
@@ -1,14 +1,14 @@
 package com.connector.qq;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableOAuth2Sso
-public class MainApp extends SpringBootServletInitializer  {
+public class MainApp extends SpringBootServletInitializer {
 
     /*
      * Uncomment main method and delete SpringBootServletInitializer to package as .jar / stand alone micro-service.
