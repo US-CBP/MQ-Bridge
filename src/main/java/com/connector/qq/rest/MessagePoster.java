388f78046715d03691721cde6e0e26362638eb6f
Robert Stine <RobWStine@outlook.com>
2018-12-07 16:26:50 -0500

    Updating Main

--- src/main/java/com/connector/qq/rest/MessagePoster.java
+++ src/main/java/com/connector/qq/rest/MessagePoster.java
@@ -1,3 +1,8 @@
+/*
+ * All Application code is Copyright 2016, The Department of Homeland Security (DHS), U.S. Customs and Border Protection (CBP).
+ *
+ * Please see LICENSE.txt for details.
+ */
 package com.connector.qq.rest;
 
 import com.connector.qq.model.MessagePayload;
