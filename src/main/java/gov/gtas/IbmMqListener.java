7fa0a5e8aead9a27e28f0abf6cb377c6d42bf4f1
Robert Stine <RobWStine@outlook.com>
2019-06-01 21:56:48 -0400

    Update IbmMqListener to support 3 types of JMS messages.

--- src/main/java/gov/gtas/IbmMqListener.java
+++ src/main/java/gov/gtas/IbmMqListener.java
@@ -5,6 +5,7 @@
  */
 package gov.gtas;
 
+import com.ibm.msg.client.wmq.WMQConstants;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +13,8 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Component;
 
-import javax.jms.BytesMessage;
-import javax.jms.JMSException;
-import javax.jms.Message;
-import javax.jms.TextMessage;
+import javax.jms.*;
+import java.io.UnsupportedEncodingException;
 import java.util.UUID;
 
 @Component
@@ -49,7 +48,11 @@ public class IbmMqListener {
             }
         } catch (Exception io) {
             logger.error("Error posting payload. Will now write message contents to error folder and try again later.", io);
-            fileReader.writeErrorFile(contents);
+            if (contents != null) {
+                fileReader.writeErrorFile(contents);
+            } else {
+                logger.error("File contents are null. Not writing to error queue.");
+            }
         }
     }
 
@@ -57,23 +60,31 @@ public class IbmMqListener {
         String contents = null;
         try {
             contents = getContentsAsString(message);
-        } catch (JMSException e) {
-            logger.error("Unable to get file contents");
+        } catch (JMSException | UnsupportedEncodingException e) {
+            logger.error("Unable to get file contents", e);
         }
         return contents;
     }
 
-    private String getContentsAsString(Message message) throws JMSException {
+    private String getContentsAsString(Message message) throws JMSException, UnsupportedEncodingException {
         String contents = null;
         if (message instanceof BytesMessage) {
             BytesMessage bytesMessage = (BytesMessage) message;
             byte[] data = new byte[(int) bytesMessage.getBodyLength()];
             bytesMessage.readBytes(data);
             bytesMessage.reset();
-            contents =  new String(data);
+            String codePage = message.getStringProperty(WMQConstants.JMS_IBM_CHARACTER_SET);
+            if (codePage != null) {
+                contents =  new String(data, codePage);
+            } else {
+                contents = new String(data);
+            }
         } else if (message instanceof TextMessage) {
             TextMessage textMessage = (TextMessage) message;
             contents = textMessage.getText();
+        } else if (message instanceof ObjectMessage) {
+            ObjectMessage omi = (ObjectMessage)message;
+            contents = (String)omi.getObject();
         }
         return contents;
     }
